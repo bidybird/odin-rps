f0ec89f6583789f6ad38803854c19990b38fe242
Jonathan Shovak <shovakitup@gmail.com>
2022-09-28 12:35:35 -0500

    made array for comp choice and variable for random answer

--- rps-script.js
+++ rps-script.js
@@ -2,7 +2,15 @@
 
 
 // Computer picks rock paper or scissors randomly
+function getComputerChoice(compRpsAnswer) {
+    let rpsArray [                //Make an array of choices to pick
+        "rock",
+        "paper",
+        "scissors"
+    ];
 
+    let = compRpsAnswer = rpsArray[Math.floor(Math.random()*rpsArray.length)]; //Pick a word from the array
+}
 
 // RPS answers are compared
 
