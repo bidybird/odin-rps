11027c35246b9f2180c7ce21668c3fc1d7f68cbd
Jonathan Shovak <shovakitup@gmail.com>
2022-09-28 12:54:19 -0500

    Console logs compRpsAnswer

--- rps-script.js
+++ rps-script.js
@@ -1,18 +1,20 @@
-// Player picks rock paper or scissors
+// Part 1 Player picks rock paper or scissors
 
 
-// Computer picks rock paper or scissors randomly
+// Part 2 Computer picks rock paper or scissors randomly
 function getComputerChoice(compRpsAnswer) {
-    let rpsArray [                //Make an array of choices to pick
+    let rpsArray = [
         "rock",
         "paper",
         "scissors"
     ];
 
     let = compRpsAnswer = rpsArray[Math.floor(Math.random()*rpsArray.length)]; //Pick a word from the array
-}
+    console.log(compRpsAnswer); // Display Computers RPS answer on console
+};
+        //Need to be able to reference answer outside of function
 
-// RPS answers are compared
+// Part 3 RPS answers are compared
 
 
-// Display game results
+// Part 4 Display game results
