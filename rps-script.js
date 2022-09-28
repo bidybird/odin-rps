9b90f26504e9ab7e9a6e3e4f4824a5acc61f10f8
Jonathan Shovak <shovakitup@gmail.com>
2022-09-28 13:01:19 -0500

    Subdivided part 1 code, players choice

--- rps-script.js
+++ rps-script.js
@@ -1,12 +1,16 @@
 // Part 1 Player picks rock paper or scissors
+    // prompt appears
+    // player writes choice in prompt
+    // choice is converted into uppercase letters
+
 
 
 // Part 2 Computer picks rock paper or scissors randomly
 function getComputerChoice(compRpsAnswer) {
     let rpsArray = [
-        "rock",
-        "paper",
-        "scissors"
+        "ROCK",
+        "PAPER",
+        "SCISSORS"
     ];
 
     let = compRpsAnswer = rpsArray[Math.floor(Math.random()*rpsArray.length)]; //Pick a word from the array
